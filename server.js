d92f0bbde8c74db1b0ece45bf394232f42be4501
achmad raizaldi <achmad.raizaldi@gmail.com>
2024-10-29 21:57:56 +0700

    menambahkan file koneksi dan file response

---
+++ server.js
@@ -0,0 +1,12 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+
+//parse application/json
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+ app.listen(3000, ()=>{
+    console.log(`server started on port`)
+ });
