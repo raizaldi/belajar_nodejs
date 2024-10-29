39e37cb5a0893c4220cfffd1c28a11d3a6cab6ea
achmad raizaldi <achmad.raizaldi@gmail.com>
2024-10-29 22:14:25 +0700

    menampilkan data mahasiswa dari database

--- controller.js
+++ controller.js
@@ -6,3 +6,13 @@ var connection = require('./koneksi');
 exports.index = function(req,res){
     response.ok('aplikasi rest berjalan baik',res);
 }
+
+exports.tampilkanmahasiswa = function(req,res){
+    connection.query('SELECT * FROM mahasiswa', function(error,rows, fields){
+        if(error){
+            console.log(error)
+        }else{
+            response.ok(rows,res);
+        }
+    })
+}
--- routes.js
+++ routes.js
@@ -5,4 +5,7 @@ module.exports = function(app){
 
     app.route('/')
         .get(jsonku.index);
+
+    app.route('/tampilkanmahasiswa')
+        .get(jsonku.tampilkanmahasiswa);
 }
